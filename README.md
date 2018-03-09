8ab1cd535396ade37c52dd2054ac900b1925ffcb
Paljor129 <pgyatso333@gmail.com>
2018-03-09 11:38:45 +0100

    commit base

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # demo-heroku
+To deploy an app with heroku
+In heroku create new app by clicking on new
+Choose github connect to github
+Search your repo and connect
+Then tape on Enable automatic deploys
+Deploy the express server with heroku and open the app in heroku 
+If we have to deploy front with back then we have to do npm install and npm run build in front folder
+And we can also do npm install and npm run build automatic by putting  "postinstall": "cd front && npm install && npm run build" in script of main package.json
 
-Deploy the express server with heroku
-
+if we had a problems then we can put key as NPM_CONFIG_PRODUCTION and value as false in config vars in settings
--- index.js
+++ index.js
@@ -1,18 +1,50 @@
+require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { makeExecutableSchema } = require('graphql-tools')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const cors = require('cors')
+const path = require('path')
+
+
+import schema from './server/schema'
+import resolvers from './server/resolvers'
+import { Person } from'./server/model'
+
 const app = express()
 
-//if there is anything after / then it will go to search in /front/build
-app.use('/', express.static(__dirname + '/front/build'))
 
-app.get('/', (req, res) => {
-    //dirname take the root file of the machine or app
-    res.sendFile(__dirname + '/front/build/index.html')
+mongoose.connect(
+    process.env.DATABASE_URL,
+    err => {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("mongoose OK")
+    }
+)
+
+
+const execSchemas = makeExecutableSchema({
+    typeDefs: schema,
+    resolvers
 })
-console.log(__dirname)
 
-//port if u find a port otherwise 3000
-const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log('Connected')
+
+app.use(
+    '/graphql', bodyParser.json(), graphqlExpress({ schema:  execSchemas, context: { Person }}))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+app.use("/", express.static(path.join(__dirname, "front/build")))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front/build/index.html'))
 })
+console.log('DIRNAME : ', __dirname)
+
+
+
+
+app.listen(3001, () => console.log('apps running on port 3001'))
